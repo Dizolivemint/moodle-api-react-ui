b3fa2a8dee9c2b190bc2fcfcce07ba1ee591de54
Miles Exner <dizolivemint@gmail.com>
2020-05-20 14:51:09 -0700

    Add component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import GetUsers from './components/GetUsers'
+import GetCourses from './components/GetCourses'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <GetUsers />
+        <GetCourses />
       </header>
     </div>
   );
