6e469f20ca1ca3460fe7430597a526450d89b83a
Miles Exner <dizolivemint@gmail.com>
2020-05-21 16:05:56 -0700

    Add Redux Store Provider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux'
+import configureStore from './store/configureStore'
 import AdminUi from './components/AdminUi'
+
+const store = configureStore()
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store} className="App">
       <header className="App-header">
         <AdminUi />
       </header>
-    </div>
+    </Provider>
   );
 }
 
